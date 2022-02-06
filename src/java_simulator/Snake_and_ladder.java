4fb16027b7e98c1b4910487088ced64afe36c502
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-07 03:35:19 +0530

    added: snak and lader

---
+++ src/java_simulator/Snake_and_ladder.java
@@ -0,0 +1,11 @@
+package java_simulator;
+
+public class Snake_and_ladder {
+ 
+  public static void main(String[] args) {
+	
+	  int start = 0;
+	  System.out.println("starting position");
+  }	
+	
+}
