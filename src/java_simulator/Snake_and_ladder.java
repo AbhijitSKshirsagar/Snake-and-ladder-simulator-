0d616c3f44bbbf030e92f7703a6a46c3d70f2326
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-07 16:00:43 +0530

    added: snak and ladder position

--- src/java_simulator/Snake_and_ladder.java
+++ src/java_simulator/Snake_and_ladder.java
@@ -4,12 +4,35 @@ public class Snake_and_ladder {
  
   public static void main(String[] args) {
 	
-	  int start =0;
+	  int position =0;
       
-	  System.out.println("Starting position: " + start);
-		int roll = 1 +(int) Math.floor(Math.random()*6);
-		System.out.println("Dice rolled: " +roll);
+	  System.out.println("position is " + position);
+		int die=(int) (Math.random() * 10) % 6+1;
+			System.out.println("dice value is " + die);
+		int value=(int) (Math.random() * 10) % 3+1;
+		System.out.println("case number is " + value);
+		 switch(value) {
+		   case 1:
+		   System.out.println( "No Play You are in the same position." );
+		   break;
+		  
+		   case 2:
+		   position = (position + die);
+		   System.out.println( "your position after ladder is ::" + position );
+		   break;
+		   
+		   case 3: 
+		   position = (position - die);
+		  
+		  if(position<0){
+			  position = 0;
+		  }
+			 System.out.println( "Your Position after snake is::" + position );
+		   break;
+		   }
+		 }
+	
   
   }	
 	
-}
+
