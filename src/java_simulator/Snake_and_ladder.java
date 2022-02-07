09059e766c61db4cbc32ec54f6070ccfd334b61e
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-02-07 14:05:43 +0530

    added: role the die

--- src/java_simulator/Snake_and_ladder.java
+++ src/java_simulator/Snake_and_ladder.java
@@ -5,7 +5,11 @@ public class Snake_and_ladder {
   public static void main(String[] args) {
 	
 	  int start =0;
-	  System.out.println("starting position is:" +start);
+      
+	  System.out.println("Starting position: " + start);
+		int roll = 1 +(int) Math.floor(Math.random()*6);
+		System.out.println("Dice rolled: " +roll);
+  
   }	
 	
 }
